d706e584c2dddd3925cba721e6cd1997169e4539
Rajesh Salian <rajeshsalian@rocketmortgage.com>
2023-12-03 16:51:32 -0500

    updated springjdbc transaction project

--- Spring_jdbc/springjdbctransaction/src/main/java/com/example/demo/AppDAO.java
+++ Spring_jdbc/springjdbctransaction/src/main/java/com/example/demo/AppDAO.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
